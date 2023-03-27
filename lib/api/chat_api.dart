1435b29588a7d539a297df391c142e075a6220a5
zhichao jiang <zhichaojiang@zhichao-MacBook-Air.local>
2023-03-28 08:53:50 +0900

    fix bug that state is not saved persisted

--- lib/api/chat_api.dart
+++ lib/api/chat_api.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:chat_ease/models/chat_message.dart';
+import 'package:chat_ease/utils/logger_util.dart';
 import 'package:dart_openai/openai.dart';
-import 'package:chat_ease/store/shared_preferences_manager.dart';
+import 'package:chat_ease/utils/shared_preferences_util.dart';
 
 class ChatApi {
   Future<String> completeChat(List<ChatMessage> messages) async {
     OpenAI.apiKey =
-        await (await SharedPreferencesManager.getInstance()).getApiKey();
-    OpenAI.organization =
-        await (await SharedPreferencesManager.getInstance()).getApiOrg();
+        SharedPreferencesUtil.prefs.getString('open_ai_api_key') ?? '';
+    OpenAI.organization = SharedPreferencesUtil.prefs.getString('open_ai_org');
 
     final chatCompletion = await OpenAI.instance.chat.create(
-        model: await (await SharedPreferencesManager.getInstance()).getModel(),
+        model: SharedPreferencesUtil.prefs.getString('open_ai_model') ??
+            'gpt-3.5-turbo',
         messages: messages
             .map((e) => OpenAIChatCompletionChoiceMessageModel(
                 role: e.isUserMessage ? 'user' : 'assistant',
@@ -23,14 +23,18 @@ class ChatApi {
   Future<bool> validateApiKey(String key) async {
     try {
       OpenAI.apiKey = key;
+      print('key: $key');
       OpenAI.organization =
-          await (await SharedPreferencesManager.getInstance()).getApiOrg();
+          SharedPreferencesUtil.prefs.getString('open_ai_org');
 
-      await OpenAI.instance.completion.create(
-          model:
-              await (await SharedPreferencesManager.getInstance()).getModel());
-    } on RequestFailedException catch (_, e) {
-      // TODO add to error log
+      await OpenAI.instance.chat.create(
+          model: SharedPreferencesUtil.prefs.getString('open_ai_model') ??
+              'gpt-3.5-turbo',
+          messages: [
+            OpenAIChatCompletionChoiceMessageModel(role: 'user', content: 'Hi')
+          ]);
+    } catch (e) {
+      LoggerUtil.logError(e.toString());
       return false;
     }
     return true;
